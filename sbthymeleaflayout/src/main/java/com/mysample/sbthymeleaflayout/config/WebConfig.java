314e2c54b924b1fce788c0fa43f202d994863b63
brientoh <boh@pristinecore.com>
2015-11-13 15:03:03 -0800

    first commit

---
+++ sbthymeleaflayout/src/main/java/com/mysample/sbthymeleaflayout/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.mysample.sbthymeleaflayout.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mysample.sbthymeleaflayout")
+@PropertySource("classpath:application.properties")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
